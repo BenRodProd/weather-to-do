b712465a5d72e86b02a82fd0ba4326f687e161ed
BenRodProd <125862830+BenRodProd@users.noreply.github.com>
2023-09-19 12:39:31 +0200

    cleaned up

--- src/app/components/LoadingScreen.js
+++ src/app/components/LoadingScreen.js
@@ -1,21 +1,10 @@
-import styled from 'styled-components';
-import { Spinner } from '@nextui-org/spinner';
 import Image from 'next/image';
 
-const StyledLoadingScreen = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  width: 100%;
-  background-color: white;
-  z-index: 6;
-`;
+
 
 export default function LoadingScreen() {
   return (
     <div className="loading">
-      
       <Image src="/Spinner.gif" alt="loading" width="80" height="80" />
     </div>
   );
