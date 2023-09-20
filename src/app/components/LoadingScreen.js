6917d90a85d2aa6ec952ecfaad2bd07c604876d4
BenRodProd <125862830+BenRodProd@users.noreply.github.com>
2023-09-20 13:16:47 +0200

    fixed spinner

--- src/app/components/LoadingScreen.js
+++ src/app/components/LoadingScreen.js
@@ -5,7 +5,7 @@ import Image from 'next/image';
 export default function LoadingScreen() {
   return (
     <div className="loading">
-      <Image src="/Spinner.gif" alt="loading" width="80" height="80" />
+      <Image src="/spinner.gif" alt="loading" width="80" height="80" />
     </div>
   );
 }
